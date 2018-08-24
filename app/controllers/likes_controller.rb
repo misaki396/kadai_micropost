aa927f30cff135517dd11291bd2264edbc1aa27b
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2018-08-24 05:26:55 +0000

    okiniiri2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,7 +9,7 @@ class LikesController < ApplicationController
   
 
   def destroy
-    @like = Like.find(
+    @like = Like.find_by(
       user_id: current_user.id,
       micropost_id: params[:micropost_id])
     @like.destroy
