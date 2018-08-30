73320fc467fad1ea502d405ca8f615dbff9efcb4
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2018-08-30 13:48:20 +0000

    sakujokinoutuika

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,6 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :users, through: :likes
 end
