b360d6cae8dfcd6d333bfa41f6b77e4235152eb3
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2018-08-31 04:40:54 +0000

    indentseiri5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
