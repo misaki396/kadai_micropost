a002ec2db75b9317f22eb3a776f1f718cbc113ea
徳藤美咲 <tokufujimisaki@tokufujimisakinoMacBook.local>
2018-10-24 18:49:40 +0900

    star-5

--- app/assets/javascripts/star.js
+++ app/assets/javascripts/star.js
@@ -1,76 +1,83 @@
 $(function(){
+    var misaki = 0
+
     $('.js-star').each(function(i){
         $(this).attr('id', 'js-star-id' + (i+1));
+        misaki = i+1
     });
-});
 
-//$(function(){
-//    $('.fas').each(function(i){
-//        $(this).attr('id','element' + (i+1));
-//    });
-//});
+    console.log(misaki)
 
+    for (var p=1; p<=misaki; p++){
+        (function(){
+        var misaki2 = '#js-star-id' + p
+        console.log(misaki2)
+            $(misaki2 + ' > .star_1').on('click',function(){
+                if ($(misaki2 + ' > .star_1').css('color') == 'rgb(255, 0, 0)'){
+                    $(misaki2 + ' > .star_1').css('color', 'yellow')
+                    console.log('ok');
+                } else {
+                    console.log('星が黄色だったら')
+                    $(misaki2 + ' > .star_2').css('color', 'red')
+                    $(misaki2 + ' > .star_3').css('color', 'red')
+                    $(misaki2 + ' > .star_4').css('color', 'red')
 
-$("[id^=js-star-id1] [class=]").on('click', function() {
-    if($('.star_1').css('color') == 'rgb(255, 0, 0)'){
-    $('.star_1').css('color', 'yellow')
-    console.log('ok');
-    } else {
-    console.log('星が黄色だったら')
-        $('.star_2').css('color', 'red')
-        $('.star_3').css('color', 'red')
-        $('.star_4').css('color', 'red')
-        $('.star_5').css('color', 'red')
+                    $(misaki2 + ' > .star_5').css('color', 'red')
+                }
+            });
+
+            $(misaki2 + ' > .star_2').on('click',function(){
+                if($(misaki2 + ' > .star_2').css('color') == 'rgb(255, 0, 0)'){
+                    $(misaki2 + ' > .star_1').css('color', 'yellow')
+                    $(misaki2 + ' > .star_2').css('color', 'yellow')
+                    console.log('ok');
+                } else {
+                    console.log('星が黄色だったら')
+                    $(misaki2 + ' > .star_3').css('color', 'red')
+                    $(misaki2 + ' > .star_4').css('color', 'red')
+                    $(misaki2 + ' > .star_5').css('color', 'red')
+                }
+            });
+
+            $(misaki2 + ' > .star_3').on('click',function(){
+                if($(misaki2 + ' > .star_3').css('color') == 'rgb(255, 0, 0)'){
+                    $(misaki2 + ' > .star_1').css('color', 'yellow')
+                    $(misaki2 + ' > .star_2').css('color', 'yellow')
+                    $(misaki2 + ' > .star_3').css('color', 'yellow')
+                    console.log('ok');
+                } else {
+                    console.log('星が黄色だったら')
+                    $(misaki2 + ' > .star_4').css('color', 'red')
+                    $(misaki2 + ' > .star_5').css('color', 'red')
+                }
+            });
+
+            $(misaki2 + ' > .star_4').on('click',function(){
+                if($(misaki2 + ' > .star_4').css('color') == 'rgb(255, 0, 0)'){
+                    $(misaki2 + ' > .star_1').css('color', 'yellow')
+                    $(misaki2 + ' > .star_2').css('color', 'yellow')
+                    $(misaki2 + ' > .star_3').css('color', 'yellow')
+                    $(misaki2 + ' > .star_4').css('color', 'yellow')
+                    console.log('ok');
+                } else {
+                    console.log('星が黄色だったら')
+                    $(misaki2 + ' > .star_5').css('color', 'red')
+                }
+            });
+
+            $(misaki2 + ' > .star_5').on('click',function(){
+                if($(misaki2 + ' > .star_5').css('color') == 'rgb(255, 0, 0)'){
+                    $(misaki2 + ' > .star_1').css('color', 'yellow')
+                    $(misaki2 + ' > .star_2').css('color', 'yellow')
+                    $(misaki2 + ' > .star_3').css('color', 'yellow')
+                    $(misaki2 + ' > .star_4').css('color', 'yellow')
+                    $(misaki2 + ' > .star_5').css('color', 'yellow')
+                }
+            });
+
+        })(p)
+
     }
-    });
-
-$(document).on('click', '.star_2', function() {
-    if($('.star_2').css('color') == 'rgb(255, 0, 0)'){
-    $('.star_1').css('color', 'yellow')
-    $('.star_2').css('color', 'yellow')
-    console.log('ok');
-    } else {
-        console.log('星が黄色だったら')
-          $('.star_3').css('color', 'red')
-          $('.star_4').css('color', 'red')
-          $('.star_5').css('color', 'red')}
-    });
-
-$(document).on('click', '.star_3', function() {
-    if($('.star_3').css('color') == 'rgb(255, 0, 0)'){
-    $('.star_1').css('color', 'yellow')
-    $('.star_2').css('color', 'yellow')
-    $('.star_3').css('color', 'yellow')
-    console.log('ok');
-    } else {
-        console.log('星が黄色だったら')
-    $('.star_4').css('color', 'red')
-    $('.star_5').css('color', 'red')}
-    });
 
 
-$(document).on('click', '.star_4', function() {
-    if($('.star_4').css('color') == 'rgb(255, 0, 0)'){
-    $('.star_1').css('color', 'yellow')
-    $('.star_2').css('color', 'yellow')
-    $('.star_3').css('color', 'yellow')
-    $('.star_4').css('color', 'yellow')
-    console.log('ok');
-    } else {
-        console.log('星が黄色だったら')
-        $('.star_5').css('color', 'red')
-    }
-});
-
-$(document).on('click', '.star_5', function() {
-    if($('.star_5').css('color') == 'rgb(255, 0, 0)'){
-    $('.star_1').css('color', 'yellow')
-    $('.star_2').css('color', 'yellow')
-    $('.star_3').css('color', 'yellow')
-    $('.star_4').css('color', 'yellow')
-    $('.star_5').css('color', 'yellow')
-    console.log('ok');
-    } else {
-        console.log('星が黄色だったら')
-    }
 });
